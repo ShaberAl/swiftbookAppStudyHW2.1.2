391494ec1df16427ffbc480947ed7252b605ce50
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-12-01 17:09:15 +0500

    Add logic, minor design changes. Ready

--- swiftbookAppStudyHW2.1.2/ViewController.swift
+++ swiftbookAppStudyHW2.1.2/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
     }
-
-
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redView.alpha != 1.0 && yellowView.alpha != 1.0 {
+            makeAllViewsSemiVisible()
+            redView.alpha = 1.0
+        } else if yellowView.alpha != 1.0 {
+            makeAllViewsSemiVisible()
+            yellowView.alpha = 1.0
+        } else if greenView.alpha != 1.0 {
+            makeAllViewsSemiVisible()
+            greenView.alpha = 1.0
+        }
+    }
+    
+    private func configureViews() {
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+    }
+    
+    private func makeAllViewsSemiVisible() {
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+    }
 }
 
